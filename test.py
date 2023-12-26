eb015408690f3c1960806004dd55050928bd651b
Manik121133 <154758199+Manik121133@users.noreply.github.com>
2023-12-26 03:33:08 -0800

    a file

---
+++ test.py
@@ -0,0 +1,52 @@
+#word="anaconda"
+#for letter in word:
+	#print (letter)
+#lop2,,,,,,,,,
+
+#words= ["Apple", "Banana", "Car", "Dolphin" ]
+#for word in words:
+	#print (word)
+# lop3,,,,,,,_________
+
+#nums = (1, 2, 3, 4)
+
+#sum_nums = 0
+
+#for num in nums:
+    #sum_nums = sum_nums + num
+
+#print(f'Sum of numbers is/ {sum_nums}')
+# for 4 ,,,,,,,,________
+
+#a = "manik"
+#for i in range(a*5):
+    #print(i)
+# foe lop 5_______
+
+#for i in range(5):
+    #print("my fras progrmmer")
+# for 6__________
+
+#m = "bangldesh"
+#for i in range(8):
+    #print(m)
+# for 7___________
+
+#m = 8
+#for i in range(m): 
+    #print("*"*i)
+
+# for 8____________
+def a (e):
+    if e > 0:
+        for i in range (e,0,-1):
+            print ("*"*i)
+            a (e-9)
+#a(5)
+#a(4)
+a(6)
+
+
+	 
+
+
